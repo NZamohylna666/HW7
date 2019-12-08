5958b4c59b9238196f8c808c206e90931792c8b4
NZamohyna666 <Zamohylnana@gmail.com>
2019-12-08 15:33:48 +0200

    cat commit

---
+++ Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    String name;
+
+
+    public void eat(int food) {
+       System.out.println(name + " ate " + food + " portions");
+    }
+}
+"# HW7" 
---
+++ HwWithCat.java
@@ -0,0 +1,14 @@
+public class HwWithCat {
+    public static void main(String[] args) {
+
+    String name;
+     Cat myCat = new Cat();
+     myCat.name = "Markiz";
+
+        myCat.eat(5);
+
+
+
+}
+}
+"# HW7" 
